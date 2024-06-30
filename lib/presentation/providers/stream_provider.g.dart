c65fccb72b43c1a41ecfcf4ff1d58c466a2ce54b
Daniel Brizuela <carlosbrizuelamereles@gmail.com>
2024-06-30 00:02:29 -0400

    chore: Add stream provider to providers.dart and stream_provider_screen.dart

---
+++ lib/presentation/providers/stream_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'stream_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userInChatHash() => r'ca6995bbad68ab0813da1a219dac57d712221b45';
+
+/// See also [userInChat].
+@ProviderFor(userInChat)
+final userInChatProvider = StreamProvider<List<String>>.internal(
+  userInChat,
+  name: r'userInChatProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$userInChatHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef UserInChatRef = StreamProviderRef<List<String>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../providers/stream_provider.dart';
 
 class StreamProviderScreen extends StatelessWidget {
   const StreamProviderScreen({super.key});
@@ -9,6 +11,71 @@ class StreamProviderScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Stream Provider'),
+      ),
+      body: const StreamView(),
+    );
+  }
+}
+
+// manera de hacer 1
+// class StreamView extends ConsumerWidget {
+//   const StreamView({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final usersInChatAsync = ref.watch(userInChatProvider);
+
+//     if (usersInChatAsync.hasError) {
+//       return const Center(
+//         child: CircularProgressIndicator(),
+//       );
+//     }
+
+//     if (usersInChatAsync.isLoading) {
+//       return const Center(
+//         child: CircularProgressIndicator(),
+//       );
+//     }
+
+//     final users = usersInChatAsync.value!;
+
+//     return ListView.builder(
+//       itemCount: users.length,
+//       itemBuilder: (BuildContext context, int index) {
+//         final user = users[index];
+//         return ListTile(
+//           title: Text(user),
+//         );
+//       },
+//     );
+//   }
+// }
+
+// manera de hacer 2
+class StreamView extends ConsumerWidget {
+  const StreamView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final usersInChatAsync = ref.watch(userInChatProvider);
+
+    return usersInChatAsync.when(
+      data: (users) {
+        return ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (BuildContext context, int index) {
+            final user = users[index];
+            return ListTile(
+              title: Text(user),
+            );
+          },
+        );
+      },
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+      error: (error, stack) => Center(
+        child: Text('Error: $error'),
       ),
     );
   }
